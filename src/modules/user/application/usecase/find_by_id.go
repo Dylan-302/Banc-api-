113bfd517b8e9a0ad78856931114f4f57053058e
Dylan <165859680+Dylan-302@users.noreply.github.com>
2026-06-17 15:45:36 +0000

    feat/find_by_id

--- src/modules/user/application/usecase/find_by_id.go
+++ src/modules/user/application/usecase/find_by_id.go
@@ -1,1 +1,19 @@
 package usecase
+
+import (
+	"banc-api/src/modules/user/domain/entities"
+	"banc-api/src/modules/user/domain/repositories"
+)
+
+func FindByIDUsecase(id uint, repo repositories.UserRepository) (*entities.User, error) {
+	user, err := repo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		return nil, nil
+	}
+
+	return user, nil
+}
--- src/modules/user/handlers/find_by_id.go
+++ src/modules/user/handlers/find_by_id.go
@@ -1,1 +1,39 @@
 package handlers
+
+import (
+	"strconv" // Sirve para convertir texto a número
+
+	"banc-api/src/modules/user/application/usecase"
+	"banc-api/src/modules/user/domain/repositories"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type SearchUserHandler struct {
+	repo repositories.UserRepository
+}
+
+func NewSearchUserHandler(repo repositories.UserRepository) *SearchUserHandler {
+	return &SearchUserHandler{repo: repo}
+}
+
+func (h *SearchUserHandler) Handle(c fiber.Ctx) error {
+	idParam := c.Params("id")
+
+	id, err := strconv.ParseUint(idParam, 10, 32)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "El ID proporcionado no es válido",
+		})
+	}
+
+	user, err := usecase.FindByIDUsecase(uint(id), h.repo)
+	if err != nil {
+		// Si el caso de uso devuelve un error (usuario no encontrado)
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(user)
+}
